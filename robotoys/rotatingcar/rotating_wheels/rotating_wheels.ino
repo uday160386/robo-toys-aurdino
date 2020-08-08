ed3b682db56229d205cefba90593b1d3080bb9a6
BK Venkata UdayKumar <31610853+uday160386@users.noreply.github.com>
2020-08-08 14:20:47 +0530

    Update rotating_wheels.ino

--- robotoys/rotatingcar/rotating_wheels/rotating_wheels.ino
+++ robotoys/rotatingcar/rotating_wheels/rotating_wheels.ino
@@ -2,7 +2,7 @@ char t;
 
 void setup() {
   pinMode(10, OUTPUT);  //left motors forward
-  pinMode(11, OUTPUT);  //left motors reverse
+  pinMode(8, OUTPUT);  //left motors reverse
   pinMode(12, OUTPUT);  //right motors forward
   pinMode(13, OUTPUT);  //right motors reverse
 
@@ -18,35 +18,35 @@ void loop() {
 
   if (t == '1') {          //move forward(all motors rotate in forward direction)
     digitalWrite(10, HIGH);
-    digitalWrite(11, HIGH);
-    digitalWrite(12, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(12, HIGH);
     digitalWrite(13, LOW);
   }
 
   else if (t == '2') {    //move reverse (all motors rotate in reverse direction)
     digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(12, LOW);
     digitalWrite(13, HIGH);
   }
 
   else if (t == '3') {    //turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(12, LOW);
     digitalWrite(13, LOW);
   }
 
   else if (t == '4') {    //turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
     digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(13, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
   }
 
   else if (t == '5') {    //STOP (all motors stop)
     digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
+    digitalWrite(8, LOW);
     digitalWrite(12, LOW);
     digitalWrite(13, LOW);
   }
